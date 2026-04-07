8a9c42f4b538d0ce537f98eea986f6238a4601b5
Аrinabitcoin <arina.sokolova2007@gmail.com>
2026-04-07 22:53:11 +0300

    #11 fixed files

--- src/models/team.py
+++ src/models/team.py
@@ -19,7 +19,7 @@ class TeamStatusEnum(str, Enum):
 @dataclass
 class Role:
     """Роль в команде"""
-    id: UUID = field(default_factory=uuid4)
+    id: UUID
     name: str = ""
     description: str = ""
     count: int = 1
@@ -28,7 +28,7 @@ class Role:
 @dataclass
 class User:
     """Пользователь системы"""
-    id: UUID = field(default_factory=uuid4)
+    id: UUID 
     name: str = ""
     surname: str = ""
     patronymic: str = ""
@@ -37,15 +37,14 @@ class User:
 @dataclass
 class Team:
     """Команда"""
-    id: UUID = field(default_factory=uuid4)
+    id: UUID 
     name: str = ""
     description: str = ""
-    track_id: UUID = field(default_factory=uuid4)
-    event_id: UUID = field(default_factory=uuid4)
-    owner_id: UUID = field(default_factory=uuid4)
-    member_ids: List[UUID] = field(default_factory=list)
-    required_roles: List[Role] = field(default_factory=list)
+    track_id: UUID 
+    event_id: UUID 
+    owner_id: UUID 
+    member_ids: list[UUID] 
+    required_roles: list[Role] 
     status: TeamStatusEnum = TeamStatusEnum.DRAFT
-    created_at: datetime = field(default_factory=datetime.now)
-    updated_at: datetime = field(default_factory=datetime.now)
-
+    created_at: datetime 
+    updated_at: datetime
