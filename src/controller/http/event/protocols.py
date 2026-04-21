b6a73c04cfe2b0723a8c1d95ff981e3e7a518a3a
mrsskaa <mariabyrtseva@yandex.ru>
2026-04-21 15:53:32 +0300

    fix trubles

--- src/controller/http/event/protocols.py
+++ src/controller/http/event/protocols.py
@@ -1,7 +1,5 @@
 import uuid
-from typing import Protocol, Optional
-
-import fastapi
+from typing import Optional, Protocol
 
 from src.models.event import Event as EventModel, Participant as ParticipantModel
 
@@ -15,8 +13,21 @@ class EventService(Protocol):
 
     async def get_event_by_id(self, event_id: uuid.UUID) -> EventModel: ...
 
-    async def get_participants(self, event_id: uuid.UUID) -> list[ParticipantModel]: ...
+    async def get_events_page(
+        self,
+        event_id: Optional[uuid.UUID] = None,
+        offset: Optional[int] = None,
+        limit: int = 10,
+    ) -> tuple[list[EventModel], Optional[uuid.UUID]]: ...
 
-    async def create_participant(self, participant: ParticipantModel) -> uuid.UUID: ...
+    async def add_participant(
+        self, event_id: uuid.UUID, participant: ParticipantModel
+    ) -> None: ...
 
-    async def get_events(self, event_id: Optional[uuid.UUID] = None, offset: Optional[int] = None, limit: int = 10) -> list[EventModel]: ...
+    async def get_participants(
+        self,
+        event_id: uuid.UUID,
+        offset: Optional[int] = None,
+        participant_id: Optional[uuid.UUID] = None,
+        limit: int = 10,
+    ) -> tuple[list[ParticipantModel], Optional[uuid.UUID]]: ...
